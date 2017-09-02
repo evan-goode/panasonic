3a5a25028207660627d4e222c4268cda9bb9d43c
Evan Goode <mail@evangoo.de>
2017-09-02 14:05:29 -0400

    new gurgle, fixed whole thing

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,75 @@
 const gulp = require('gulp');
 
-const gulpUtil = require('gulp-util');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
+const util = require('gulp-util');
+const del = require('del')
 const pug = require('gulp-pug');
+const autoprefixer = require('gulp-autoprefixer');
+const jade = require('gulp-pug');
 const babel = require('gulp-babel');
 const notify = require('gulp-notify');
+const sass = require('gulp-sass');
+
+const source = "source";
+const build = "build";
+const gulpSrcOptions = {base: source};
 
 const paths = {
-  sass: ['**/*.sass', '**/*.scss', '!node_modules/**'],
-  pug: ['**/*.jade', '**/*.pug', '!node_modules/**'],
-  babel: ['**/*.babel', '!node_modules/**']
+	sass: [`${source}/**/*.sass`, `${source}/**/*.scss`],
+	pug: [`${source}/**/*.pug`, `${source}/**/*.jade`],
+	babel: [`${source}/**/*.babel`]
 };
-
-function errorHandler (error) {
-  gulpUtil.log(gulpUtil.colors.red(error.message));
-  this.emit('end');
+paths.generic = [`${source}/**/*`];
+for (group in paths) {
+	if (group !== "generic") {
+		for (path of paths[group]) {
+			paths.generic.push('!' + path);
+		}
+	}
 }
 
-gulp.task('default', ['sass', 'pug', 'babel']);
-
+const handleError = function (error) {
+	util.log(util.colors.red(error.message));
+	this.emit('end');
+}
+gulp.task('default', ['sass', 'pug', 'babel', 'generic']);
 gulp.task('sass', () => {
-  return gulp.src(paths.sass)
-    .pipe(sass())
-    .on('error', errorHandler)
-    .pipe(autoprefixer())
-    .on('error', errorHandler)
-    .pipe(gulp.dest('.'))
-		.pipe(notify("<%= file.relative %>"));
+	return gulp.src(paths.sass, gulpSrcOptions)
+		.pipe(sass())
+		.on('error', handleError)
+		.pipe(autoprefixer())
+		.on('error', handleError)
+		.pipe(gulp.dest(build))
+		.pipe(notify('<%= file.relative %>'));
 });
-
 gulp.task('pug', () => {
-  return gulp.src(paths.pug)
-    .pipe(pug())
-    .on('error', errorHandler)
-    .pipe(gulp.dest('.'))
-		.pipe(notify("<%= file.relative %>"));
+	return gulp.src(paths.pug, gulpSrcOptions)
+		.pipe(pug())
+		.on('error', handleError)
+		.pipe(gulp.dest(build))
+		.pipe(notify('<%= file.relative %>'));
+});
+gulp.task('babel', () => {
+	return gulp.src(paths.babel, gulpSrcOptions)
+		.pipe(babel({
+			presets: ['env']
+		}))
+		.on('error', handleError)
+		.pipe(gulp.dest(build))
+		.pipe(notify('<%= file.relative %>'));
+});
+gulp.task('generic', () => {
+	return gulp.src(paths.generic, gulpSrcOptions)
+		.pipe(gulp.dest(build));
+		// .pipe(notify('<%= file.relative %>'));
 });
 
-gulp.task('babel', () => {
-  return gulp.src(paths.babel)
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .on('error', errorHandler)
-    .pipe(gulp.dest('.'))
-		.pipe(notify("<%= file.relative %>"));
+gulp.task('clean', () => {
+	return del(`${build}/*`);
 });
 
 gulp.task('watch', () => {
-  gulp.watch(paths.sass, ['sass']);
-  gulp.watch(paths.pug, ['pug']);
-  gulp.watch(paths.babel, ['babel']);
+	gulp.watch(paths.sass, ['sass']);
+	gulp.watch(paths.pug, ['pug']);
+	gulp.watch(paths.babel, ['babel']);
+	gulp.watch(paths.generic, ['generic']);
 });
---
+++ source/panasonic.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+import requests
+from string import digits
+digits += "." # for floats
+import json
+from random import choice
+
+output = "cheapest.json"
+products = {
+    "B003EM8008": "black",
+    "B003ELYQJI": "red",
+    "B003EM6AOG": "blue",
+    "B003ELYQHK": "green",
+    "B003ELYQGG": "orange",
+    "B003EM2WAW": "pink",
+    "B003EM800S": "silver",
+    "B003EM6AQE": "violet"
+}
+base_url = "https://smile.amazon.com/dp"
+user_agents = [
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/602.4.8 (KHTML, like Gecko) Version/10.0.3 Safari/602.4.8",
+	"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
+]
+colors = []
+for product, color in products.items():
+    user_agent = choice(user_agents)
+    print(f"requesting for {product}")
+    url = "/".join([base_url, product])
+    request = requests.get(url, headers = {"User-Agent": user_agent})
+    soup = BeautifulSoup(request.text, "html.parser")
+    price_text = soup.select("#price .a-size-medium")[0].text
+    price = float("".join([character for character in price_text if character in digits]))
+    colors.append({
+        "color": color,
+        "price_text": price_text,
+        "price": price,
+        "link": url,
+    })
+
+cheapest = min(colors, key=lambda color: color["price"])
+print(cheapest)
+with open(output, "w") as pointer:
+    json.dump(cheapest, pointer)
